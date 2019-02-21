6e85e5bda2be633b106ebade1b5c66367a6ad341
Henrik Finsberg <henriknf@simula.no>
2019-02-21 10:20:09 +0100

    Add demo folders to sys.path when running them in CI

--- tests/run_all_demos.py
+++ tests/run_all_demos.py
@@ -1,4 +1,8 @@
 import os
+import sys
+# There are some problems with some plotting backends in containers.
+# Therefore we set the backend here, and execute the files in stead
+# of spawning subprocesses.
 import matplotlib
 matplotlib.use("agg")
 
@@ -15,4 +19,10 @@ for root, dirname, files in os.walk(demodir):
             print(root)
             print(f)
             os.chdir(root)
+            # Add the current folder to sys.path so that
+            # python finds the relevant modules
+            sys.path.append(root)
+            # Execute file
             exec(open(f).read())
+            # Remove the current folder from the sys.path
+            sys.path.pop()
