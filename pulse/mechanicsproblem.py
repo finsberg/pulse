bbeeb25182d7ed3d8a888d5da712dbaf564d59ff
Alexandra Diem <alexandra.diem@gmail.com>
2019-09-25 10:56:15 +0200

    fixed merge issue

--- pulse/mechanicsproblem.py
+++ pulse/mechanicsproblem.py
@@ -159,8 +159,6 @@ class MechanicsProblem(object):
                 self.bcs_parameters = MechanicsProblem.default_bcs_parameters()
                 if bcs_parameters is not None:
                     self.bcs_parameters.update(**bcs_parameters)
-                self.bcs_parameters.update(**bcs_parameters)
-
             else:
                 raise ValueError(
                     ("Please provive boundary conditions " "to MechanicsProblem")
