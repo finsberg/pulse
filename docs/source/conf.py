e8a1a830f2973207b162d74aa502b5c91fdfeff3
Henrik Finsberg <henriknf@simula.no>
2021-05-13 21:31:04 +0200

    Try to add a few more configurations to sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,6 @@ from pathlib import Path
 from textwrap import dedent
 from unittest import mock
 
-import plotly  # noqa: F401
-
 sys.modules["dolfin"] = mock.Mock()
 
 import pulse  # noqa: E402
@@ -115,12 +113,23 @@ with open(demo_docs.joinpath("demos.rst"), "w+") as f:
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
     "sphinx.ext.mathjax",
+    "sphinx.ext.graphviz",
+    "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
     "nbsphinx",
 ]
+
+try:
+    import matplotlib.sphinxext.plot_directive  # noqa: F401
+
+    extensions.append("matplotlib.sphinxext.plot_directive")
+except ImportError:
+    pass
+
 
 # Enable plotly figure in the docs
 nbsphinx_prolog = r"""
@@ -171,7 +180,8 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "dist", "**.ipynb_checkpoints"]
+
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
