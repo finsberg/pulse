ffe0a567698b1f8d2170a490bba34085bca214db
Henrik Finsberg <henriknf@simula.no>
2021-11-22 15:10:19 +0100

    Fix test for demos

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -1,5 +1,4 @@
 import os
-import subprocess as sp
 import sys
 from pathlib import Path
 from unittest import mock
@@ -28,27 +27,24 @@ demos = [
     (f, root)
     for root, dirname, files in os.walk(demodir)
     for f in files
-    if Path(f).suffix == ".ipynb" and "checkpoint" not in Path(f).name
+    if Path(f).suffix == ".py"
 ]
 
 
 @pytest.mark.parametrize("filename, root", demos)
 def test_demo(filename, root):
-    if os.path.basename(root) == "closed_loop":
+    if os.path.basename(root) in ["closed_loop", "optimal_control"]:
         return
     if os.path.basename(root) == "creating_geometries" and not has_mshr:
         return
     os.chdir(root)
     # Add the current folder to sys.path so that
     # python finds the relevant modules
-
-    sp.check_call(["jupytext", filename, "--to", ".py"])
-    py_filename = Path(filename).with_suffix(".py")
+    py_filename = Path(filename)
     sys.path.append(root)
     # Execute file
     with mock.patch("pulse.mechanicsproblem.MechanicsProblem.solve") as solve_mock:
         solve_mock.return_value = (1, True)  # (niter, nconv)
         exec(open(py_filename).read(), globals())
     # Remove the current folder from the sys.path
-    py_filename.unlink()
     sys.path.pop()
