b9de8d117390c5f7e3491ecfe0231e014a123569
finsberg <finsberg@users.noreply.github.com>
2021-11-23 07:09:33 +0000

    deploy: ae99b3fd4260d411bb0edd4f98bb22a84279e124
    Former-commit-id: 02238b707db2d9d4489e39756041b419b8b74fda

--- src/constants/docs.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    FORMAT_LINK: 'https://github.com/d3/d3-3.x-api-reference/blob/master/Formatting.md#d3_format',
-    DATE_FORMAT_LINK: 'https://github.com/d3/d3-time-format#locale_format'
-};
