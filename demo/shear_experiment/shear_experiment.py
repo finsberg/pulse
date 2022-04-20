1c4540a803bd3280e0f1b8fc28f152aaf891b3ff
Henrik Finsberg <henriknf@simula.no>
2022-04-20 08:37:08 +0200

    Add plot of PK1 in shear experiment

--- demo/shear_experiment/shear_experiment.py
+++ demo/shear_experiment/shear_experiment.py
@@ -14,8 +14,11 @@ from pathlib import Path
 import dolfin
 import matplotlib.pyplot as plt
 import numpy as np
+import ufl
+import typing
 
 import pulse
+from pulse.mechanicsproblem import MechanicsProblem
 
 try:
     from dolfin_adjoint import (
@@ -37,16 +40,24 @@ except ImportError:
     )
 
 
+class Experiment(typing.NamedTuple):
+    problem: MechanicsProblem
+    increment: typing.Callable[[float], typing.Tuple[float, float, float]]
+    shear_component: typing.Callable[[ufl.tensors.ComponentTensor], float]
+
+
 # Create mesh
 N = 2
 mesh = UnitCubeMesh(N, N, N)
 
 
 # Create a facet fuction in order to mark the subdomains
+
 ffun = dolfin.MeshFunction("size_t", mesh, 2)
 ffun.set_all(0)
 
 # Mark subdomains
+
 xlow = dolfin.CompiledSubDomain("near(x[0], 0) && on_boundary")
 xlow_marker = 1
 xlow.mark(ffun, xlow_marker)
@@ -73,30 +84,34 @@ zhigh.mark(ffun, zhigh_marker)
 
 
 # Collect the functions containing the markers
+
 marker_functions = pulse.MarkerFunctions(ffun=ffun)
 
 
 # Create mictrotructure
+
 V_f = dolfin.VectorFunctionSpace(mesh, "CG", 1)
 
-# Fibers
+# Fibers, sheets and fiber-sheet normal
+
 f0 = interpolate(Expression(("1.0", "0.0", "0.0"), degree=1), V_f)
-# Sheets
 s0 = interpolate(Expression(("0.0", "1.0", "0.0"), degree=1), V_f)
-# Fiber-sheet normal
 n0 = interpolate(Expression(("0.0", "0.0", "1.0"), degree=1), V_f)
 
 # Collect the mictrotructure
+
 microstructure = pulse.Microstructure(f0=f0, s0=s0, n0=n0)
 
 # Create the geometry
+
 geometry = pulse.Geometry(
     mesh=mesh,
     marker_functions=marker_functions,
     microstructure=microstructure,
 )
 
-# Use the default material parameters
+# Use the default material parameters from the paper
+
 material_parameters = {
     "a": 0.059,
     "b": 8.023,
@@ -109,11 +124,17 @@ material_parameters = {
 }
 
 # Create material
+
 material = pulse.HolzapfelOgden(parameters=material_parameters)
+
+# Make a space for controling the amount of shear displacement
 
 X_space = dolfin.VectorFunctionSpace(mesh, "R", 0)
 x = Function(X_space)
 zero = Constant((0.0, 0.0, 0.0))
+
+
+# Make a method that will return
 
 
 def create_experiment(case):  # noqa: C901
@@ -215,34 +236,59 @@ def create_experiment(case):  # noqa: C901
     bcs = pulse.BoundaryConditions(dirichlet=(dirichlet_bc,))
 
     # Create problem
-    return pulse.MechanicsProblem(geometry, material, bcs), increment, shear_component
+    return Experiment(
+        problem=pulse.MechanicsProblem(geometry, material, bcs),
+        increment=increment,
+        shear_component=shear_component,
+    )
 
 
-stress: dict[str, list[float]] = {}
+# Loop over all modes and collect the stress values
+
+modes = ["fs", "fn", "sf", "sn", "nf", "ns"]
+stress: dict[str, dict[str, list[float]]] = {
+    "cauchy": {m: [] for m in modes},
+    "pk1": {m: [] for m in modes},
+}
 shear_values = np.linspace(0, 0.6, 10)
-
 recompute = True
 # Solve problem
 results_file = Path("result.npy")
 if recompute or not results_file.is_file():
-    for mode in ["fs", "fn", "sf", "sn", "nf", "ns"]:
+    for mode in modes:
         x.assign(zero)
-        stress[mode] = []
-        problem, increment, shear_component = create_experiment(mode)
+        experiment = create_experiment(mode)
 
         for shear in shear_values:
-            print(increment(shear))
-            pulse.iterate.iterate(problem, x, increment(shear), reinit_each_step=True)
-            stress[mode].append(shear_component(problem.ChachyStress()))
+            pulse.iterate.iterate(
+                experiment.problem,
+                x,
+                experiment.increment(shear),
+                reinit_each_step=True,
+            )
+            stress["cauchy"][mode].append(
+                experiment.shear_component(experiment.problem.ChachyStress()),
+            )
+            stress["pk1"][mode].append(
+                experiment.shear_component(experiment.problem.FirstPiolaStress()),
+            )
 
     np.save(results_file, stress)
+
+# Plot results
+
 stress = np.load(results_file, allow_pickle=True).item()
-fig, ax = plt.subplots()
-for mode, values in stress.items():
-    ax.plot(shear_values, stress[mode], label=mode)
-ax.set_ylabel("Shear stress (kPa)")
-ax.set_xlabel("Amount of shear")
-ax.set_ylim((0, 16))
-ax.grid()
-ax.legend()
+fig, ax = plt.subplots(1, 2, sharey=True, sharex=True)
+for mode, values in stress["cauchy"].items():
+    ax[0].plot(shear_values, stress["cauchy"][mode], label=mode)
+ax[0].set_title("Cauchy Stress")
+for mode, values in stress["pk1"].items():
+    ax[1].plot(shear_values, stress["pk1"][mode], label=mode)
+ax[1].set_title("First Piola Kirchhoff Stress")
+ax[0].set_ylabel("Shear stress (kPa)")
+for axi in ax:
+    axi.set_xlabel("Amount of shear")
+    axi.set_ylim((0, 16))
+    axi.grid()
+    axi.legend()
 plt.show()
